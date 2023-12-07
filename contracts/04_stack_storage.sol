a25ef078c92a716e50e82f5edb60a7d65743f094
tianq02 <1019235014@qq.com>
2023-12-07 11:30:28 +0800

    merge

--- contracts/04_stack_storage.sol
+++ contracts/04_stack_storage.sol
@@ -7,12 +7,12 @@ contract StackStorage {
     // 尝试提升信任度时，以renonce为起始，逐个尝试，直到投入了指定工作量，记录期间获得最大信任度的nonce，更新nonce
     // 完成指定工作量后，在renonce中记录当前尝试的nonce，即使没有得到更高信任度的值也更新
     // 注：如果使用随机化算法，或许不需要renonce
-    struct Closure {
-        string  headhash;
-        string  endhash;
-        uint256 nonce;
-        bool isValid;
-    }
+    // struct Closure {
+    //     string  headhash;
+    //     string  endhash;
+    //     uint256 nonce;
+    //     bool isValid;
+    // }
 
     // 存储数据的映射
     // 哈希表结构，本程序实现对栈数据结构的模拟
@@ -55,7 +55,7 @@ contract StackStorage {
     }
 
     // 修改对应下标处的数据
-    function updateDataAtIndex(uint256 _index, string memory _data) public isNotOutBound(_index) {
+    function updateDataAtIndex(uint256 _index, string memory _data) public isNotOutBound(_index) isNotExist(_data) {
         string memory oldData = closures[_index];
         delete indexes[keccak256(abi.encode(oldData))];
 
@@ -80,35 +80,36 @@ contract StackStorage {
         return curIndex;
     }
 
+
     // 检查栈是否为空
     modifier isNotEmpty() {
-        require(curIndex >= 1, "stack empty");
+        require(curIndex > 1, "stack empty");
         _;
     }
 
     // 检查请求的下标是否越界
     modifier isNotOutBound(uint256 _index) {
-        require(curIndex >= 1, "stack empty");
-        require(_index < curIndex && _index >= 0, "index outbound");
+        require(curIndex > 1, "stack empty");
+        require(_index < curIndex && _index > 0, "index outbound");
         _;
     }
 
-    // 数据是否重复
+    // 数据不存在
     modifier isNotExist(string memory _data) {
         require(indexes[keccak256(abi.encode(_data))]==0, "data already exist");
         _;
     }
 
+    // 数据已存在
     modifier isExist(string memory _data) {
-        uint256 index = indexes[keccak256(abi.encode(_data))];
-        // bool isDataEmpty = closures[index] == '';
-        require(index !=0 , "data not exist");
+        require(indexes[keccak256(abi.encode(_data))]!=0, "data not exist");
         _;
     }
 
     // 初始化，设置栈为空
     constructor() {
-        curIndex = 0;
+        // 约定栈的下标从1开始，0越界
+        curIndex = 1;
     }
 
 
@@ -140,47 +141,21 @@ contract StackStorage {
     }
 
     // 2. 获取closure数据
-    function getClosure(uint256 _closureIndex) public view returns(string memory) {
+    function getClosureByIndex(uint256 _closureIndex) public view returns(string memory) {
         return getDataAtIndex(_closureIndex);
     }
 
+    // 3.                                                                                                                                               
+
     // 3. 计算closure的信任度
-    function getTrust(Closure memory _data) public pure returns (uint256) {
-        // 计算 sha256(endhash+nonce)
-        Closure memory data = _data;
-        bytes32 calculatedHash = sha256(abi.encodePacked(data.endhash, data.nonce));
+    function getTrust(string memory closureData, uint256 nonce) public {
 
-        // 将字符串转换为字节数组，以便进行按位比较
-        bytes memory headBytes = bytes(data.headhash);
-        bytes32 headHashBytes;
-
-        // 确保 headBytes 的长度小于等于 32
-        require(headBytes.length <= 32, "Headhash.len>32");
-
-        // 使用零填充 headBytes，创建一个32字节的数组
-        for (uint256 i = 0; i < headBytes.length; i++) {
-            headHashBytes |= bytes32(uint256(uint8(headBytes[i])) << (8 * (headBytes.length - 1 - i)));
-        }
-
-        // 执行按位比较并计数连续匹配的位数
-        uint256 matchingBits = 0;
-        uint256 mask = 1;
-        for (uint256 i = 0; i < 256; i++) {
-            if ((uint256(calculatedHash) & (mask << i)) == (uint256(headHashBytes) & (mask << i))) {
-                matchingBits++;
-            } else {
-                break;
-            }
-        }
-
-        return matchingBits;
     }
 
-
     // 4. 更新Closure（加信任度）
-    function updateClosure(string memory oldData, string memory newData) public {
-        uint256 index = getIndexOfData(oldData);
-        updateDataAtIndex(index, newData);
+    function updateClosure(string memory _oldData, string memory _newData) public isExist(_oldData) isNotExist(_newData) {
+        uint256 index = getIndexOfData(_oldData);
+        updateDataAtIndex(index, _newData);
     }
 
 }
